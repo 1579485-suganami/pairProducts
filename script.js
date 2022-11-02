f78df93f4063bdb3da8595445ecc38c8f74f5e32
1579485-suganami <suganami,atsushi@mail.toyota.co.jp>
2022-11-02 21:15:52 +0900

    feat: add  images/markdown & changed script.js etc

--- script.js
+++ script.js
@@ -16,29 +16,48 @@ function omikuzi() {
         ]
         const rmdNum = Math.floor(Math.random() * kuziList.length);
         const kuziEl = document.createElement("div");
-        // kuziEl.className = "pokeClass";
         kuziEl.innerText ="今日の運勢は" + kuziList[rmdNum] + "です" + "\n" + kuziComent[rmdNum];
         kuziEl.style.color = "white"
 
         const nameEl = document.createElement("div");
         nameEl.innerText = "lucky Pokemon: " + json.name;
         nameEl.style.color = "white"
+
+        const buttonEl = document.createElement("button");
+        buttonEl.id = "pokeButton";
+        buttonEl.setAttribute("onclick", "anime()");
+        buttonEl.width = 300;
+        buttonEl.height = 300;
+        buttonEl.style.borderRadius = "150px";
         
         const imgEl = document.createElement('img');
-
         imgEl.src = json.sprites.front_default;
         imgEl.width = 300;
         imgEl.style.borderRadius = "150px";
         const rgbArr = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)]
         imgEl.style.backgroundColor = `rgb(${rgbArr[0]}, ${rgbArr[1]}, ${rgbArr[2]})`
         
+        buttonEl.appendChild(imgEl);
+
         const pokemonEl = document.getElementById("luckypokemon")
         pokemonEl.innerText = ""
         pokemonEl.width = 300
         pokemonEl.append(kuziEl);
         pokemonEl.append(nameEl);
-        pokemonEl.append(imgEl);
-    } )
-    
+        pokemonEl.append(buttonEl);
+    })  
 };
+function anime() {
+    const target = document.getElementById("pokeButton");
+    console.log(target);
+    target.animate(
+        [
+            { transform: "rotate(360deg)" } ,
+        ], 
+        {
+            duration: 100,
+            iterations: Infinity
+        }
+    );
+}
 
